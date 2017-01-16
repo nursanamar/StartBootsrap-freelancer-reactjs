77458f91acdb046e599001a079c8ad3cf7c556f4
Nursan amar <nursan011@gmail.com>
2017-01-16 00:03:12 +0000

    render sampai contact

--- js/app.js
+++ js/app.js
@@ -43,10 +43,162 @@ function Header() {
     </header>;
 }
 function PortofolioSection() {
+	return <section id="portfolio">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <h2>Portfolio</h2>
+                    <hr className="star-primary" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/cabin.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/cake.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/circus.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal4" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/game.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal5" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/safe.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal6" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/submarine.png" className="img-responsive" alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>;
+}
+function AboutSection() {
+   return <section className="success" id="about">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <hr className="star-light" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-4 col-lg-offset-2">
+                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
+                </div>
+                <div className="col-lg-4">
+                    <p>Whether youre a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
+                </div>
+                <div className="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="#" className="btn btn-lg btn-outline">
+                        <i className="fa fa-download"></i> Download Theme
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>;
+}
+function ContactSection() {
+	return  <section id="contact">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <h2>Contact Me</h2>
+                    <hr className="star-primary" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <div className="row control-group">
+                            <div className="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" className="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div className="row control-group">
+                            <div className="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" className="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div className="row control-group">
+                            <div className="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Phone Number</label>
+                                <input type="tel" className="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div className="row control-group">
+                            <div className="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" className="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br />
+                        <div id="success"></div>
+                        <div className="row">
+                            <div className="form-group col-xs-12">
+                                <button type="submit" className="btn btn-success btn-lg">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>;
+}
+function Footer() {
 	return 
 }
 function App() {
-	return <div><Navigation /><Header /> </div>;
+	return <div><Navigation /><Header /><PortofolioSection /><AboutSection /><ContactSection /></div>;
 }
 ReactDOM.render(
 	<App />,
