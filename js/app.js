ce44136d7e73ac09f455044aef177ef8d647f87a
Nursan amar <nursan011@gmail.com>
2017-01-18 11:06:26 +0800

    scroll button

--- js/app.js
+++ js/app.js
@@ -258,13 +258,14 @@ function App(props) {
 	<AboutSection />
 	<ContactSection />
 	<Footer />
+	<ScrollButton />
 	{modal}
 	</div>;
 }
 var data =[{
 	"id":"PortofolioModal1","judul":"Project Title",
 "img":"img/portfolio/cabin.png",
-"description":"Use this area of the page to describe your project. The icon above is part of a free icon set by <a href='https://sellfy.com/p/8Q9P/jV3VZ/'>Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!"
+"description":"Use this area of the page to describe your project. The icon above is part of a free icon set by <a href=https://sellfy.com/p/8Q9P/jV3VZ/>Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!"
 },{
 	"id":"PortofolioModal2","judul":"Project Title",
 "img":"img/portfolio/cake.png",
